a81de2399716049cbee622fc7e8faf374e64ef54
mysubmin <gnasus2884@naver.com>
2024-01-15 08:33:11 +0900

    Initial Commit

---
+++ git-test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  git-test
+//
+//  Created by t2023-m0039 on 1/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
